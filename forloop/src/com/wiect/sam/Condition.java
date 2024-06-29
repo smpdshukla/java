3d52022574dd096689b9746ca028443f05cee3de
Lenovo <Lenovo@DESKTOP-F0EP5EI>
2024-06-29 16:41:43 +0530

    This is a program based on for loop which multiplies numbers from 20 to 10.

---
+++ forloop/src/com/wiect/sam/Condition.java
@@ -0,0 +1,20 @@
+package com.wiect.sam;
+
+public class Condition {
+
+	public static void main(String[] args) {
+	
+		for(int j=20;j<=10;j++)
+		{
+			for(int i=1;i<=10;i++)
+			{
+				System.out.println(j + "x" + i + "=" + (i*j));
+			}
+			System.out.println("====================");
+			
+			
+		}
+
+	}
+
+}
